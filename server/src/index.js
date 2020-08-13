58fb307d5ea3044bb5c4b6d903b4d5854f85b328
Umang Patel <umang25011@gmail.com>
2020-08-13 20:40:19 +0530

    Added jwt token for authentication, set jwt in cookie

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const app = express()
+const cookieParser = require("cookie-parser")
+
 const userRouter = require("./routes/user")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
-app.use("/user", userRouter)
+app.use("/api/user", userRouter)
 
+// so server response won't send the type of backend( express )
 // MANAGE ALL INVALID ROUTER IN invalid.js ROUTES FILE
 
 app.listen(process.env.PORT, () =>
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -2,10 +2,15 @@ const router = require("express").Router()
 const axios = require("axios")
 const { redirectURL, githubAuth } = require("../middleware/githubAuth")
 const database = require("../database/db")
+const cookieParser = require("cookie-parser")
+
+// require jwtAuth function, getTOken is to get new tokens, authenticate will very the tokens
+const { authenticate, getToken } = require("../middleware/jwtauth")
 
 var db // store db object in this object
 database.connectDB(() => (db = database.getDb("users")))
 
+// it has now use now, the redirectURL will be embedded in client side,
 router.get("/login", (req, res) => {
 	res.redirect(redirectURL)
 })
@@ -15,21 +20,25 @@ router.get("/github", githubAuth, async (req, res) => {
 
 	//check if user exists
 	try {
-		const isUser = await db.findOne({ email: req.user.data.email })
-		if (isUser) {
-			return res.send(isUser)
+		let userId = await db.findOne(
+			{ email: req.user.data.email },
+			{ projection: { _id: 1 } }
+		)
+		//if user does not exist, create a new user
+		if (!userId) {
+			user = {
+				name: req.user.data.name,
+				username: req.user.data.login,
+				email: req.user.data.email,
+				githubProfile: req.user.data.html_url,
+				avatar: req.user.data.avatar_url,
+			}
+			user = await db.insertOne(user)
+			userId = user.insertedId
 		}
-
-		// create a new user
-		const user = {
-			name: req.user.data.name,
-			username: req.user.data.login,
-			email: req.user.data.email,
-			githubProfile: req.user.data.html_url,
-			avatar: req.user.data.avatar_url,
-		}
-		await db.insertOne(user)
-		res.send(user)
+		const token = getToken(userId)
+		res.cookie("jwt", token, { httpOnly: true, sameSite: true })
+		res.send("Login Successful")
 	} catch (error) {
 		res.status(500).send("Github authentication Failed, Try again!")
 		console.log(error)
